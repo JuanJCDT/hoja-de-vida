ad95e0230b0bd3024df4b92820e693152a70a80d
JuanJCDT <79609160+JuanJCDT@users.noreply.github.com>
2021-02-25 20:35:43 -0500

    Add files via upload

---
+++ Hoja de vida/enviar.php
@@ -0,0 +1,26 @@
+<?php
+
+$nombre = $_POST['nombre']
+$email = $_POST['email']
+$numero = $_POST['numero']
+$mensaje = $_POST['mensaje']
+
+$header = 'From: ' . $email . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mine-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por: " . $nombre . " \r\n";
+$mensaje .= "Su email es: " . $email . " \r\n";
+$mensaje .= "Telefono de contacto: " . $numero . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "enviado el: " . date('d/m/Y' , time());
+
+$para = 'juanca200043@gmail.com';
+$asunto = 'Hoja de vida web'
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:index.html")
+
+?>
